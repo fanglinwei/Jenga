77b44de37577ccb1c66d5df350284ae9076a849a
fun <lw_fun@163.com>
2022-07-06 19:47:23 +0800

    优化InsetGrouped

--- InsetGrouped/InsetGrouped/AppDelegate.swift
+++ InsetGrouped/InsetGrouped/AppDelegate.swift
@@ -14,21 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        JengaEnvironment.setup()
-        
-        JengaEnvironment.autoTable { frame in
-            let tableView: UITableView
-            if #available(iOS 13.0, *) {
-                tableView = UITableView(frame: frame, style: .insetGrouped)
-            } else {
-                tableView = UITableView(frame: frame, style: .grouped)
-                tableView.cc_isInsetGrouped = true
-                tableView.cc_insetGroupedHorizontalInset = 16
-                tableView.cc_cornerRadius = 8
-            }
-            return tableView
-        }
-        
+        JengaEnvironment.isEnabledLog = true
+        JengaEnvironment.setup(JengaProvider())
         return true
     }
 
@@ -44,5 +31,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+struct JengaProvider: Jenga.JengaProvider {
+    
+    func defaultTableView(with frame: CGRect) -> UITableView { 
+        let tableView: UITableView
+        if #available(iOS 13.0, *) {
+            tableView = UITableView(frame: frame, style: .insetGrouped)
+        } else {
+            tableView = UITableView(frame: frame, style: .grouped)
+            tableView.cc_isInsetGrouped = true
+            tableView.cc_insetGroupedHorizontalInset = 16
+            tableView.cc_cornerRadius = 8
+        }
+        return tableView
     }
 }
